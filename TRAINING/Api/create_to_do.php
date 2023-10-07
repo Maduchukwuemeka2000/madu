a517bb6e045c5d6aa018c989bc440f89d32137bf
emekus <maduchukwuemeka.mi@gmail.com>
2023-10-07 18:28:14 +0100

    adjsuted code

--- TRAINING/Api/create_to_do.php
+++ TRAINING/Api/create_to_do.php
@@ -1,7 +1,7 @@
 <?php
 //note:the To_Do list has only the task and the time to carry out such task
 
-header("Access-Control-Allow-Oringin : *");
+header("Access-Control-Allow-Oringin : * ");
 header("Content-Type : application/json");
 header("Access-Control-Allow-Methods : POST");
 
@@ -20,7 +20,7 @@ $testing = new Query($db);
 $testing->To_Do = isset($_GET['to_do']) ? $_GET['to_do'] : die();
 $time = isset($_GET['time']) ?  new DateTime(str_replace('/' , '-' , $_GET['time'])) : new DateTime($testing->time);
 $testing->time = $time->format('Y-m-d H:i:s');
-$testing->Create_To_Do( );
+$testing->Create_To_Do();
 
 
 //displying all the items
