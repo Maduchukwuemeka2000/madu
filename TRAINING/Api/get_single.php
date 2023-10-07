f27daf33d9eb61f0ab964197c866aa57184f428b
madu chukwuemeka <maduchukwuemeka.mi@gmail.com>
2023-10-07 10:24:51 +0100

    to_do table

---
+++ TRAINING/Api/get_single.php
@@ -0,0 +1,25 @@
+<?php
+//note:the To_Do list has only the task and the time to carry out such task
+
+header("Access-Control-Allow-Oringin : *");
+header("Content-Type : application/json");
+header("Access-Control-Allow-Methods : GET");
+
+require_once '../Config/Db.php';
+require_once '../Model/Query.php';
+
+//database connection
+$db = Db::con() ;
+
+//instantiating CRUD
+$testing = new Query($db);
+
+
+//getting the Id
+$testing->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+
+echo json_encode($testing->Read_single());
+
+
+
---
+++ TRAINING/Api/update.php
@@ -0,0 +1,32 @@
+<?php
+//note:the To_Do list has only the task and the time to carry out such task
+
+header("Access-Control-Allow-Oringin : *");
+header("Content-Type : application/json");
+header("Access-Control-Allow-Methods : PUT");
+
+require_once '../Config/Db.php';
+require_once '../Model/Query.php';
+// require_once "get_all.php";
+//database connection
+$db = Db::con() ;
+
+//instantiating CRUD
+$testing = new Query($db);
+
+
+//getting the Id
+$testing->id = isset($_GET['id']) ? $_GET['id'] : die();
+$testing->To_Do = isset($_GET['to_do']) ? $_GET['to_do'] : die();
+$time = isset($_GET['time']) ?  new DateTime(str_replace('/' , '-' , $_GET['time'])): new DateTime($testing->time);
+$testing->time = $time->format('Y-m-d H:i:s');
+//checking if the update was made
+if($testing->update()==true){
+    //displying all items
+    echo json_encode($testing->Read());
+
+}
+echo ($testing->time);
+
+
+
